29472dfb86ad1f2d2fa29367196825705b39945b
VIktor <viktor345@inbox.lv>
2022-08-03 22:30:43 +0300

    test_main_page.py and main_page.py update

--- test_main_page.py
+++ test_main_page.py
@@ -8,7 +8,11 @@ def test_guest_can_go_to_login_page(browser):
     time.sleep(5)
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
     time.sleep(5)
-
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
 
 # def test_guest_can_go_to_login_page(browser):
 #     link = "http://selenium1py.pythonanywhere.com/"
@@ -17,3 +21,5 @@ def test_guest_can_go_to_login_page(browser):
 #     login_link.click()
 
 #     тест без Page Object
+
+# pytest -v --tb=line --language=en test_main_page.py
